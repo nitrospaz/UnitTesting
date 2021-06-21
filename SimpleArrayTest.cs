e210efe2d45b113618018aa2f2f45730bac7b9ca
Walker Crystal <nitrospaz@hotmail.com>
2021-06-21 11:34:03 -0700

    Add files via upload

---
+++ SimpleArrayTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTest
+{
+	[TestClass]
+	public class SimpleArrayTest
+	{
+		[TestMethod]
+		public void TestInstantiation()
+		{
+			var testInstance = new SimpleArray();
+			Assert.AreEqual(testInstance.GroceryList.Length, 4);
+			Assert.AreEqual(testInstance.GroceryList[1], "Milk");
+		}
+
+		[TestMethod]
+		public void TestToString()
+		{
+			var testInstance = new SimpleArray();
+			Assert.IsTrue(testInstance.ToString().StartsWith("There are"));
+		}
+	}
+}
